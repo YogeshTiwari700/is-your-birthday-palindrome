dc53b0a3029ce9fcfe5679fef40d91e7f853cca9
Yogesh Tiwari <106140073+YogeshTiwari700@users.noreply.github.com>
2022-10-05 02:35:54 +0530

    complete

---
+++ app.js
@@ -0,0 +1,93 @@
+function reverseStr(str) {
+    var listOfChars = str.split('');
+    var reverseListOfChars = listOfChars.reverse();
+    var reversedStr = reverseListOfChars.join('');
+    return reversedStr;
+  
+  }
+  
+  function isPalindrome(str) {
+    var reverse = reverseStr(str);
+    return str === reverse;
+  }
+  
+  function convertDateToStr(date) {
+  
+    var dateStr = { day: '', month: '', year: '' };
+  
+    if (date.day < 10) {
+      dateStr.day = '0' + date.day
+    }
+    else {
+      dateStr.day = date.day.toString();
+    }
+  
+    if (date.month < 10) {
+      dateStr.month = '0' + date.month
+    }
+    else {
+      dateStr.month = date.month.toString();
+    }
+  
+    dateStr.year = date.year.toString();
+  
+    return dateStr;
+  }
+  
+  function getAllDateFormats(date) {
+    var dateStr = convertDateToStr(date);
+  
+    var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    var yyddmm = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+  
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yyddmm];
+  
+  
+  }
+  
+  function checkPalindromeForAllDateFormats(date) {
+    var listOfPalindromes = getAllDateFormats(date);
+    var itsPalindrome = false;
+  
+    for (var i = 0; i < listOfPalindromes.length; i++) {
+      if (isPalindrome(listOfPalindromes[i])) {
+        itsPalindrome = true;
+        break;
+      }
+    }
+    return itsPalindrome;
+  
+  }
+  
+  var dateInput = document.querySelector(".birth-day");
+  
+  var checkBtn = document.querySelector("#check-btn");
+  
+  var showMessage = document.querySelector("#showMessage");
+  
+  function clickHandler() {
+    var bdayStr = dateInput.value;
+    if (bdayStr !== '') {
+      var listOfDate = bdayStr.split('-');
+      var date = {
+        day: Number(listOfDate[2]),
+        month: Number(listOfDate[1]),
+        year: Number(listOfDate[0]),
+      }
+      var isPalindrome = checkPalindromeForAllDateFormats(date);
+      
+      if (isPalindrome) {
+        showMessage.innerText = "Yay  it's Your Palindrome Birthday😍."
+      }
+      else {
+        showMessage.innerText = "OOPS! it's not palindrome Birthday 😌"
+      }
+    }
+  }
+   
+    
+    checkBtn.addEventListener('click', clickHandler);
